e39e08fd33fa4c7f8a8611eec4a558f69d838d89
Rodrigo <rodrigobarbosa.p99@gmail.com>
2024-11-18 11:36:19 -0300

    feat: adicionar interfaces

---
+++ StoreSolution.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using StoreSolution.Domain.Entities;
+
+namespace StoreSolution.Domain.Interfaces;
+
+internal interface ICategoryRepository
+{
+    Task<IEnumerable<Category>> GetAllAsync();
+    Task<Category?> GetByIdAsync(Guid id);
+    Task AddAsync(Category category);
+    Task UpdateAsync(Category category);
+    Task DeleteAsync(Guid id);
+}
---
+++ StoreSolution.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using StoreSolution.Domain.Entities;
+
+namespace StoreSolution.Domain.Interfaces;
+
+internal interface IOrderRepository
+{
+    Task<IEnumerable<Order>> GetOrdersByUserAsync(Guid userId);
+    Task<Order?> GetByIdAsync(Guid id);
+    Task AddAsync(Order order);
+}
